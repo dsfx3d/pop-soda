956dd4943608ecd589312f14adbb0528c90de09a
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-04-09 20:30:09 +0530

    feat(game): add events system

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -3,11 +3,13 @@ import {ContainerProvider} from "../util/ContainerProvider";
 import {Engine, Runner} from "matter-js";
 import {ObjectManager} from "../object/ObjectManager";
 import {SceneManager} from "../scene/SceneManager";
+import {createNanoEvents} from "nanoevents";
 
 export class Game {
   readonly objects = new ObjectManager(this);
   readonly provider = new ContainerProvider(this);
   readonly scenes = new SceneManager(this);
+  readonly events = createNanoEvents();
   private runner: Runner = Runner.create({});
 
   constructor(
