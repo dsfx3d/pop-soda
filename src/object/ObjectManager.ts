bb5ceb1e3948023a46b03d1fbdb822b753b223f7
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-04-25 15:32:04 +0530

    feat(object-manager): add ability to remove all objects

--- src/object/ObjectManager.ts
+++ src/object/ObjectManager.ts
@@ -1,11 +1,10 @@
 import {AGameContainer} from "../game/AGameContainer";
 import {Container, View} from "pixi.js";
 import {IObject} from "./IObject";
-import {PoolMap} from "../util/PoolMap";
 import {World} from "matter-js";
 
 export class ObjectManager extends AGameContainer {
-  readonly pool = this.game.provider.getInstance(PoolMap<IObject>);
+  private objects: IObject[] = [];
 
   add(object: IObject) {
     object.drawable &&
@@ -15,13 +14,18 @@ export class ObjectManager extends AGameContainer {
     this.addListeners(object);
   }
 
-  remove(object: IObject) {
+  async remove(object: IObject): Promise<void> {
+    await object.exit?.();
     object.drawable &&
       // eslint-disable-next-line unicorn/prefer-dom-node-remove
       this.game.app.stage.removeChild(object.drawable as View & Container);
     object.body && World.remove(this.game.engine.world, object.body);
-    object.exit?.();
     this.removeListeners(object);
+  }
+
+  async flush(): Promise<void> {
+    await Promise.all(this.objects.map(o => this.remove(o)));
+    this.objects = [];
   }
 
   private addListeners(object: IObject) {
