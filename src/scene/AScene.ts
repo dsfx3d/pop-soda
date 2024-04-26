0f823bfd688c0549d256e3be7b1c07a6b9bcd136
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-04-26 14:38:57 +0530

    feat: streamline generics in decorators

--- src/scene/AScene.ts
+++ src/scene/AScene.ts
@@ -1,6 +1,4 @@
 import {AGameContainer} from "../game/AGameContainer";
 import {Game} from "../game/Game";
 
-export abstract class AScene<G extends Game = Game> extends AGameContainer<G> {
-  public readonly game: G;
-}
+export abstract class AScene<G extends Game = Game> extends AGameContainer<G> {}
