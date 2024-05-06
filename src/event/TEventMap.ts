d6e3a2a50aeb4ed0588f90546c6d80061553e6c0
Yashodhan Singh Rathore <dsfx3d@gmail.com>
2024-05-06 16:22:49 +0530

    feat: emit event on object entry

--- src/event/TEventMap.ts
+++ src/event/TEventMap.ts
@@ -1,10 +1,9 @@
 import {EEvent} from "./EEvent";
 import {type EventsMap} from "nanoevents";
-import {Game} from "../game/Game";
 import {IGameContainer} from "../util/IGameContainer";
 
 export type TEventMap = EventsMap & {
-  [EEvent.Exit]<G extends Game>(container: IGameContainer<G>): void;
+  [EEvent.Exit](container: IGameContainer): void;
   [EEvent.Resize](event: UIEvent): void;
-  [EEvent.Entry]<G extends Game>(container: IGameContainer<G>): void;
+  [EEvent.Entry](container: IGameContainer): void;
 };
